358766ea2de8b877d5cbc7a2471f541645e4b413
technoryta <technoryta@gmail.com>
2023-10-18 16:20:55 -0700

    updated header

--- shell.h
+++ shell.h
@@ -11,9 +11,23 @@ extern char **environ;
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <fcntl.h>
 #include <linux/limits.h>
+#include "utils_getline.c"
 
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+ssize_t _getline(char **linebuff, size_t *nbyte, FILE *stream);
+void *_realloc(void *addr, unsigned int new_sz);
+char *homedir(void);
+
+/* MAIN */
+void file_init(char *writebuff, int writeint);
+char *old_dir(void);
+void KILLIT(int indicator);
+void CTRLC(int indicator);
+
+int copystr(char *str, char *addr);
 int builtin_checker(char *command);
 int check_file_in_path(char *path);
 char **split_string(char *str, const char *delim, int *num_words);
